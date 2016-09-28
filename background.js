95b2c2f6c4d6f0942c1b30a4f8518c58f1f04726
Patrick Stockton <patrick.stockton@morleynet.com>
2016-09-28 16:12:30 -0400

    all the typings stuff

--- background.js
+++ background.js
@@ -3,4 +3,4 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   })
-})
+});
